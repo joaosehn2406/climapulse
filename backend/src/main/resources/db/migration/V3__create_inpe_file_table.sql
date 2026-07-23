f193be5840e603ee238d976f4e0114365182d296
joao.pedro <jpssantosos5874@gmail.com>
2026-07-23 19:43:11 -0300

    refact: changed file names

--- backend/src/main/resources/db/migration/V3__create_inpe_file_table.sql
+++ backend/src/main/resources/db/migration/V3__create_inpe_file_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE inpe_file (
+CREATE TABLE climapulse.inpe_file (
     filename VARCHAR(80) PRIMARY KEY,
     imported_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
--- backend/src/main/resources/db/migration/V4__create_hotspot_table.sql
+++ backend/src/main/resources/db/migration/V4__create_hotspot_table.sql
@@ -1,13 +1,19 @@
-CREATE TABLE hotspot (
+CREATE TABLE climapulse.hotspot (
     id UUID PRIMARY KEY,
-    source_filename VARCHAR(100) NOT NULL
-        REFERENCES inpe_import(filename),
+    source_filename VARCHAR(80) NOT NULL,
 
     latitude DOUBLE PRECISION NOT NULL,
     longitude DOUBLE PRECISION NOT NULL,
     satellite VARCHAR(60) NOT NULL,
-    observed_at TIMESTAMPTZ NOT NULL
-);
+    observed_at TIMESTAMPTZ NOT NULL,
 
-CREATE INDEX idx_hotspot_source_filename
-    ON hotspot(source_filename);
+    CONSTRAINT fk_hotspot_inpe_file
+        FOREIGN KEY (source_filename)
+        REFERENCES climapulse.inpe_file(filename),
+
+    CONSTRAINT ck_hotspot_latitude
+        CHECK (latitude BETWEEN -90 AND 90),
+
+    CONSTRAINT ck_hotspot_longitude
+        CHECK (longitude BETWEEN -180 AND 180)
+);
